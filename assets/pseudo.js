70e6b3f84b2ac7a05cef122ca4a0af8e21d3ab63
Drew Jordan <ajordan12@luc.edu>
2023-05-24 09:57:55 -0500

    added files

---
+++ assets/pseudo.js
@@ -0,0 +1,8 @@
+// need to add more hours for input to workday > HTML
+// display current day and time
+// functionality for the save button.. adeventListener need to store input to the local storage
+
+//create timer function 
+    // need to loop to the hur blocks based on the current time
+    // with th current time will determine what color block is needed. Add class based upon time of day
+// call on timer function to begin application 
