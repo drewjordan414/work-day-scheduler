d01a5137ed2c68a822a09b32bb060fda36b797b9
Drew Jordan <ajordan12@luc.edu>
2023-05-24 20:31:35 -0500

    started

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+document.addEventListener("DOMContentLoaded", function () {
+  window.onload = function(){
+    // come back here to add the current date to the header
+  }
+});
+
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
+  $(".saveBtn").on("click", function () {
+    var timeBlockid = $(this).parent().attr("id");
+    var textContent = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockid, textContent);
+    console.log(timeBlockid, textContent);
+  });
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
