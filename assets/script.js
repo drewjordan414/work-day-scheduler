ce0ab2f3f3459c6f01c2d2bf7f47ec165bcb38c9
Drew Jordan <ajordan12@luc.edu>
2023-05-25 19:20:03 -0500

    tryna figure it out

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,3 @@
-// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// // the code isn't run until the browser has finished rendering all the elements
-// // in the html.
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
-
 //global variables
 var currentTime = parseInt(dayjs().format("HH:MM"));
 console.log(currentTime);
@@ -28,26 +5,35 @@ var currentDay = dayjs().format("dddd, MMMM D, YYYY");
 console.log(currentDay);
 
 //functions
-function main() {
-  function checkTime() {
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+  });
+  function timeCheck() {
     for (i = 9; i < 18; i++) {
-      var hour = $("#hour-" + i);
-      var hourTime = parseInt(hour.attr("id").split("-")[1]);
-      hour.removeClass("past present future");
-
-      if(hourValue === currentTime) {
-        hour.addClass("present");
-      } else if (hourValue < currentTime) {
-        hour.addClass("past");
-      } else if (hourValue > currentTime) {
-        hour.addClass("future");
+      var hour = $("#h00" + i).attr("id");
+      if ($(hour).attr("id") === (0 + currentTime)) {
+        $(hour).addClass("present");
+      } else if (Number(($(hour).attr("id"))) < (0 + currentTime)) {
+        $(hour).addClass("past");
+      } else {
+        $(hour).addClass("future");
       }
+    }   
+  }
+  function savedItems(){
+    for (i = 9; i < 18; i++) {
+      var key = $("#h00" + i).attr("id");
+      var savedText = $(".description")
+      $("#h00" + i ).children(savedText).val(localStorage.getItem(key));
     }
-
-  }  
+  }
   $("#currentDay").text(currentDay);
-  checkTime();
+  timeCheck();
+  savedItems();
+  savedItems();
 
-};
 
-main();
+});
